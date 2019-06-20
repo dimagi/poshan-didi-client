9937c9b740d3e8762dc32ca7f34f15d529ba4cc9
Brian DeRenzi <bderenzi@dimagi.com>
2019-06-20 15:33:16 +0200

    Hardcode display to IST and source from UTC

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ Vue.config.productionTip = false
 
 Vue.filter('formatDate', function (value) {
   if (value) {
-    return moment(String(value)).format('YYYY-MM-DD hh:mm')
+    return moment(String(value)).add(330, 'minutes').format('YYYY-MM-DD HH:mm')
   }
 })
 
